53d9241d92e9e0751c222fe349941362edd1dd79
Jesus <jsuesalnugo@gmail.com>
2019-08-18 21:56:18 -0500

    crud eliminar 2

---
+++ src/app/components/lista/lista.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ConexionService } from '../../services/conexion.service';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+export class ListaComponent implements OnInit {
+
+  items:any;
+
+  constructor(private conexion:ConexionService) { 
+    this.conexion.listaItem().subscribe(item=>{
+      this.items = item;
+      console.log(this.items);
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  eliminar(item){
+   // console.log(item);
+    this.conexion.eliminarItem(item);
+  }
+
+}
